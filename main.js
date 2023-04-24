6985a7a367924b85dc1aa690672b5aa22fcf9b3a
Mariia Nikitenko <mnikitenko@LABA.PSWAS.LOCAL>
2023-04-24 15:58:26 -0700

    monday commit

---
+++ main.js
@@ -0,0 +1,30 @@
+//create some classes of what potentially we can make of :
+
+class Dish {
+    constructor (ingredient1, ingredient2){
+        this.ingredient1 = ingredient1;
+        this.ingredient2 = ingredient2
+    }
+    recipe(){return `you can cook  `}
+}
+//egg + oil = fried eggs
+//egg + milk = omelet
+//use API for recipes or create a form, that users will 
+//fill out themselves, like input=eggs, input = milk
+//OR use API https://www.themealdb.com/api.php with fetching 
+//OR https://rapidapi.com/spoonacular/api/recipe-food-nutrition/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// use adding ingredients to class
